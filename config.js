1f7449e7911af117f6d72bb42e274e5571e350be
Michał <michalstankiewicz4@gmail.com>
2026-01-30 22:56:55 +0100

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.CONFIG = {
-    deepseek_key: "sk-ab6a28540a8a4479b3b95a81344eb522",
-    gemini_key: "AIzaSyB01FepYbuUws05jseETb-dZ1hEpvd9j-o",
-    openrouter_key: "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"
-};
